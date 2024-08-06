b6ffdb3b87a68fb56aa926577901ff179f65d3da
agenthx <87952665+agenthx@users.noreply.github.com>
2024-08-06 15:58:29 +0300

    Update up.go

--- up.go
+++ up.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func Up(sentence string) (string , error){
+func UpLowCap(sentence string) (string , error){
 	arr := strings.Split(sentence, " ")
 	match := true
 	for i, word := range arr {
@@ -14,13 +14,22 @@ func Up(sentence string) (string , error){
 			return "" , fmt.Errorf("nothing before up")
 		} else if word == "(low)" && i == 0{
 			return "" , fmt.Errorf("nothing before low")
+		} else if word == "(cap)" && i == 0 {
+			return "", fmt.Errorf("nothin before cap")
 		}
 		if word == "(up)" {
 			arr[i-1] = strings.ToUpper(arr[i-1])
 			match = false
+		} else if word == "(low)" {
+			arr[i-1] = strings.ToLower(arr[i-1])
+			match = false
+		} else if word == "(cap)" {
+			arr[i-1] = strings.Title(arr[i-1])
+			match = false
 		}
 	}
 
+	//go here if you find up or low
 	if !match {
 		sentence = ""
 		for i, word := range arr {
@@ -33,6 +42,12 @@ func Up(sentence string) (string , error){
 
 	rule := regexp.MustCompile(`\(up\)`)
 	sentence = rule.ReplaceAllLiteralString(sentence, "")
+
+	rule1 := regexp.MustCompile(`\(low\)`)
+	sentence = rule1.ReplaceAllLiteralString(sentence, "")
+	
+	rule2 := regexp.MustCompile(`\(cap\)`)
+	sentence = rule2.ReplaceAllLiteralString(sentence, "")
 
 	return sentence , nil
 }
