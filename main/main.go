b217465b4542d42e88d4676c6dc0ada0857a1134
agenthx <87952665+agenthx@users.noreply.github.com>
2024-08-08 22:47:33 +0000

    lol

--- main/main.go
+++ main/main.go
@@ -1,52 +1,60 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"piscine"
+	"piscine/functions"
+	"regexp"
+	"strings"
 )
 
 func main() {
-	input := "text files/sample.txt"
-	output := "text files/result.txt"
-
-	file, err := os.Open(input)
-	if err != nil {
-		fmt.Println("Error:", err)
+	file, ferr := os.Open("text files/sample.txt") //open the file sample.txt
+	if ferr != nil {
+		panic(ferr)
 	}
 	defer file.Close()
 
-	data := make([]byte, 34)
+	scanner := bufio.NewScanner(file) //read the contents of the file and make the changes
+	var items []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		items = strings.Split(line, " ")
+		for i, word := range items {
+			if word == "(bin)" {
+				items[i-1] = piscine.Bin2Dec(items[i-1])
+			} else if word == "(hex)" {
+				items[i-1] = piscine.Hex2Dec(items[i-1])
+			} else if word == "(up)" {
+				items[i-1] = strings.ToUpper(items[i-1])
+			} else if word == "(low)" {
+				items[i-1] = strings.ToLower(items[i-1])
+			} else if word == "(cap)" {
+				items[i-1] = strings.Title(items[i-1])
+			}
+		}
+	}
+	//store the new sentence and remove the (hex), etc..
 	sentence := ""
-
-	for {
-		i, err := file.Read(data)
-		if err != nil && err.Error() != "EOF" {
-			fmt.Println("Error:", err)
-		}
-		if i == 0 {
-			break
-		}
-		sentence += string(data[:i])
+	for _, word := range items {
+		sentence += word + " "
 	}
+	regex := regexp.MustCompile(`\(bin\)|\(hex\)|\(up\)|\(low\)|\(cap\)`)
+	sentence = regex.ReplaceAllLiteralString(sentence, "")
 
-	sentence, err = piscine.UpLowCap(sentence)
+	//create result file
+	file, err := os.Create("text files/result.txt")
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
-
-	File, err := os.Create(output)
+	defer file.Close()
+	//write the sentence in the file
+	_, werr := file.WriteString(sentence)
 	if err != nil {
-		fmt.Println("Error:", err)
+		panic(werr)
 	}
-	defer File.Close()
+	defer file.Close()
 
-	_, err = File.WriteString(sentence)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	fmt.Println("Ready")
-
+	fmt.Println("READY!!!")
 }
